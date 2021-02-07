a02c7b60fc09e96eb212440484e5f22745fa9f08
erguotou <erguotou525@gmail.com>
2021-02-07 18:31:49 +0800

    feat: 生成头像coding

--- miniprogram/pages/avatar/index.ts
+++ miniprogram/pages/avatar/index.ts
@@ -44,40 +44,62 @@ Page({
   },
   saveAvatar() {
     wx.createSelectorQuery()
-      .select('.canvas')
+      .select('#canvas')
       .node()
       .exec(async res => {
         const canvas = res[0].node
         const ctx = canvas.getContext('2d')
         ctx.scale(1, 1)
-        ctx.clearRect(0, 0, 128, 128)
+        const w = 512
+        canvas.width = w
+        canvas.height = w
+        ctx.fillStyle = '#fff'
+        // 白边宽度
+        const borderW = 24
+        const radius = 8
+        // 白底
+        ctx.fillRect(0, 0, w, w)
+        // 白边裁剪区域
+        ctx.beginPath()
+        ctx.moveTo(borderW, borderW + radius)
+        ctx.lineTo(borderW, w - borderW - radius)
+        ctx.arcTo(borderW, w - borderW - radius, borderW + radius, w - borderW, radius)
+        ctx.lineTo(w - borderW - radius, w - borderW)
+        ctx.arcTo(w - borderW - radius, w - borderW, w - borderW, w - borderW - radius, radius)
+        ctx.lineTo(w - borderW, borderW + radius)
+        ctx.arcTo(w - borderW, borderW + radius, w - borderW - radius, borderW, radius)
+        ctx.lineTo(borderW + radius, borderW)
+        ctx.arcTo(borderW + radius, borderW, borderW, borderW + radius, radius)
+        ctx.closePath()
+        ctx.clip()
+
         // 头像
-        await drawImage(canvas, ctx, this.data.selectedImage || app.globalData.userInfo!.avatarUrl, 0, 0, 128, 128)
+        await drawImage(canvas, ctx, this.data.selectedImage || app.globalData.userInfo!.avatarUrl, 0, 0, 512, 512)
         // 角标外围
         ctx.beginPath()
-        ctx.arc(110, 110, 22, 0, 2 * Math.PI)
-        ctx.rect(110, 110, 22, 22)
-        ctx.fillStyle = '#fff'
+        ctx.arc(440, 440, 88, 0, 2 * Math.PI)
+        ctx.rect(440, 440, 88, 88)
         ctx.fill()
         ctx.closePath()
         // 角标
         ctx.beginPath()
-        ctx.arc(110, 110, 18, 0, 2 * Math.PI)
+        ctx.arc(440, 440, 72, 0, 2 * Math.PI)
         ctx.clip()
-        await drawImage(canvas, ctx, `../../assets/badge-${this.data.role}.png`, 92, 92, 36, 36)
+        await drawImage(canvas, ctx, `../../assets/badge-${this.data.role}.png`, 368, 368, 144, 144)
         ctx.closePath()
+        // const info = wx.getSystemInfoSync()
         wx.canvasToTempFilePath({
           // @ts-ignore
           canvas,
+          // width: 512,
+          // height: 512,
+          // destWidth: 512 * info.pixelRatio,
+          // destHeight: 512 * info.pixelRatio,
           success: (res) => {
             wx.saveImageToPhotosAlbum({
               filePath: res.tempFilePath,
               success() {
-                wx.showModal({
-                  title: '头像已保存到手机相册',
-                  content: '快去替换自己的微信头像吧',
-                  showCancel: false
-                })
+                wx.showToast({ title: '头像已保存', icon: 'success' })
               },
               fail() {
                 wx.showToast({ title: '保存失败', icon: 'none' })
