6db907f9de6b974f9c8952b2ff0227b2110807ce
shuai <1614565666@qq.com>
2022-08-10 22:23:30 +0800

    fix: 修复由于微信api调整,无法获取用户头像问题

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -3,6 +3,7 @@
 interface IAppOption {
   globalData: {
     userInfo?: WechatMiniprogram.UserInfo,
+    isHighSDKVersion?:boolean
   }
   userInfoReadyCallback?: WechatMiniprogram.GetUserInfoSuccessCallback,
 }
